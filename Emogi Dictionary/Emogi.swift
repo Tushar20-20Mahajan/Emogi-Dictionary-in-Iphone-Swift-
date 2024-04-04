3a46a85396cec8a149912e892d317f5bfab01e5c
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-04-04 10:15:41 +0530

    Changes made and emogi dictionary completed

---
+++ Emogi Dictionary/Emogi.swift
@@ -0,0 +1,8 @@
+//
+//  Emogi.swift
+//  Emogi Dictionary
+//
+//  Created by student on 03/04/24.
+//
+
+import Foundation
---
+++ Emogi Dictionary/EmojiTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmojiTableViewCell.swift
+//  Emogi Dictionary
+//
+//  Created by student on 04/04/24.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
